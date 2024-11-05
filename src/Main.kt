f15e7acbe476f1d3ee79d4a08c4a73d4797284c6
Andrés <a24andresrv@iessancelemente.net>
2024-11-05 18:40:17 +0100

    Cometarios añadidos, y correcion de detalles

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,7 @@ const val GREEN = "\u001B[32m"
 const val RESET = "\u001B[0m"
 const val BLACK = "\u001B[30m"
 const val BOLD = "\u001B[1m"
+//Función para crear un número aleatorio
 fun numerosecreto():String{
     val lista= mutableListOf(1,2,3,4,5,6)
     lista.shuffle()
@@ -14,48 +15,58 @@ fun numerosecreto():String{
     }
     return numerosecreto
 }
+//Función que se encarga del juego
 fun jugar():Unit {
     val secreto = numerosecreto()
     File("traza.txt").writeText("numero secreto: $secreto\n")
-    println(secreto)
-    var intentos = 2
+    //println(secreto)
+    //Utilicé una variable para faciliatra la modificación de la cantidad de intetnos sin tener que modificar el bucle
+    //Podría preguntarse al jugadro cunanto intentos quiere y almacenarse en la variable
+    val intentos = 8
+    print("Tienes $intentos para adivinar el número secreto.")
     var aciertos = 0
     var coincidencias = 0
-    val lectura=""
     for (i in 1 .. intentos) {
-        print("${BOLD}Teclea un número de 4 cifras sin números repetidos: ")
+        print("Teclea un número de 4 cifras sin números repetidos: ")
         val lectura = readln()
+        //ponemos a cero las variables por cada intento
         aciertos = 0
         coincidencias = 0
-        for (i in 0..secreto.lastIndex) {
-            if (secreto[i] == lectura[i]) {
+        for (j in 0..secreto.lastIndex) {
+            //si la posición de la cifra es la misma en el numero secreto que en el numero introducido sumar 1 acierto
+            if (secreto[j] == lectura[j]) {
                 aciertos++
             } else {
-                for (j in 0..lectura.lastIndex) {
-                    if (secreto[i] == lectura[j]) {
+                //Si no, recorre el numero introducido en busca de coincidecnias, esta solución es validad al no haber repeticiones de cifras.
+                for (c in 0..lectura.lastIndex) {
+                    if (secreto[j] == lectura[c]) {
                         coincidencias++
                     }
                 }
             }
         }
+        //Si adivina el numero secreto sale del bucle
         if (aciertos == secreto.length) {
             println("Has adivinado el número secreto")
             File("traza.txt").appendText("Intento $i: $lectura, Has adivinado el número secreto.\n")
             break
         }
+        //Muestr al umero introducido y los aciertos y coincidencias
         print(lectura+"   ")
         print("${BG_GREEN}${BLACK}  $aciertos  ")
         print("${BG_YELLOW}${BLACK}  $coincidencias  ${RESET}")
         println()
         File("traza.txt").appendText("Intento $i: $lectura, Aciertos: $aciertos, Coincidencias: $coincidencias\n")
     }
+    //Si al terminar los intentos no se acierta el número, mostrar este mensaje
     if(aciertos != secreto.length){
         println("Lo siento no adivinaste el número secreto $secreto en $intentos intentos")
     }
 
 }
+//Función que obtiene los datos escritos en el documento
 fun traza(){
-    var contenido=File("traza.txt").readLines()
+    val contenido=File("traza.txt").readLines()
     for (linea in contenido){
         println(linea)
     }
